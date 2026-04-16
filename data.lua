f4fa528092408e090002f37c59e5e1ce710b6ceb
Dmytro Dovgoshliubnyi <dmytro@dovgo.sh>
2026-04-16 13:42:51 +0100

    style: data and settings formatting

--- data.lua
+++ data.lua
@@ -1,7 +1,9 @@
-data:extend{
-  {
-    type = "custom-input",
-    name = "blueprint-share-send",
-    key_sequence = "CONTROL + B",
-  }
+local send_input = {
+  type = "custom-input",
+  name = "blueprint-share-send",
+  key_sequence = "CONTROL + B",
 }
+
+data:extend({
+  send_input
+})
--- settings.lua
+++ settings.lua
@@ -1,16 +1,20 @@
+local destination_port = {
+  type = "int-setting",
+  name = "blueprint-share-destination-port",
+  setting_type = "runtime-per-user",
+  default_value = 25002,
+  minimum_value = 1024,
+  maximum_value = 65535,
+}
+
+local debug = {
+  type = "bool-setting",
+  name = "blueprint-share-debug",
+  setting_type = "runtime-per-user",
+  default_value = false
+}
+
 data:extend({
-  {
-    type = "int-setting",
-    name = "blueprint-share-destination-port",
-    setting_type = "runtime-per-user",
-    default_value = 25002,
-    minimum_value = 1024,
-    maximum_value = 65535,
-  },
-  {
-    type = "bool-setting",
-    name = "blueprint-share-debug",
-    setting_type = "runtime-per-user",
-    default_value = false
-  },
+  destination_port,
+  debug,
 })
