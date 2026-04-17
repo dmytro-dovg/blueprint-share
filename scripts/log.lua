ef677415fd8b8eb2fcb51c11f0c5a33edc82f5c7
Dmytro Dovgoshliubnyi <dmytro@dovgo.sh>
2026-04-17 17:21:46 +0100

    fix: added missing log file

---
+++ scripts/log.lua
@@ -0,0 +1,53 @@
+local Log = {}
+
+local levels = {
+  quiet = 0,
+  error = 1,
+  warn = 2,
+  info = 3,
+  debug = 4,
+}
+
+local function log_to_file(level, msg)
+  if type(msg) == "string" then
+    log("blueprint-share: " .. level .. ": " .. msg)
+  elseif type(msg) == "table" then
+    log("blueprint-share: " .. level .. ": " .. msg[1])
+  end
+end
+
+local function should_print(level, player)
+  if not player then return false end
+  local setting = settings.get_player_settings(player.index)["blueprint-share-log-level"].value
+  return levels[level] <= levels[setting]
+end
+
+function Log.debug(msg, player)
+  log_to_file("debug", msg)
+  if should_print("debug", player) then
+    player.print(msg, { sound = defines.print_sound.never })
+  end
+end
+
+function Log.info(msg, player)
+  log_to_file("info", msg)
+  if should_print("info", player) then
+    player.print(msg, { sound = defines.print_sound.never })
+  end
+end
+
+function Log.warn(msg, player)
+  log_to_file("warn", msg)
+  if should_print("warn", player) then
+    player.print({"", "[color=yellow]", msg, "[/color]"})
+  end
+end
+
+function Log.error(msg, player)
+  log_to_file("error", msg)
+  if should_print("error", player) then
+    player.print({"", "[color=red]", msg, "[/color]"})
+  end
+end
+
+return Log
