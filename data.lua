000146880827d6de516dce21ab7d080b48849752
Dmytro Dovgoshliubnyi <dmytro@dovgo.sh>
2026-04-27 22:04:40 +0100

    refactor: moved inputs into a separate file

--- data.lua
+++ data.lua
@@ -1,27 +1,4 @@
+require "prototypes.inputs"
 require "prototypes.styles"
 require "prototypes.graphics"
 require "prototypes.shortcuts"
-
-local send_input = {
-  type = "custom-input",
-  name = "blueprint-share-send",
-  key_sequence = "CONTROL + B",
-}
-
-local receive_input = {
-  type = "custom-input",
-  name = "blueprint-share-receive",
-  key_sequence = "CONTROL + R",
-}
-
-local toggle_inbox_input = {
-  type = "custom-input",
-  name = "blueprint-share-toggle-inbox",
-  key_sequence = "CONTROL + SHIFT + B",
-}
-
-data:extend({
-  send_input,
-  receive_input,
-  toggle_inbox_input,
-})
---
+++ prototypes/inputs.lua
@@ -0,0 +1,23 @@
+local send_input = {
+  type = "custom-input",
+  name = "blueprint-share-send",
+  key_sequence = "CONTROL + B",
+}
+
+local receive_input = {
+  type = "custom-input",
+  name = "blueprint-share-receive",
+  key_sequence = "CONTROL + R",
+}
+
+local toggle_inbox_input = {
+  type = "custom-input",
+  name = "blueprint-share-toggle-inbox",
+  key_sequence = "CONTROL + SHIFT + B",
+}
+
+data:extend({
+  send_input,
+  receive_input,
+  toggle_inbox_input,
+})
